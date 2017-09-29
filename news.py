ffe2c1aba22d27210e6c0fb8d08011d0a8b05d02
vagrant <vagrant@vagrant.vm>
2017-09-29 16:00:15 +0000

    Modify code structure

--- news.py
+++ news.py
@@ -4,68 +4,64 @@ import psycopg2
 
 DBNAME = 'news'
 
+def db_handler(query):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    p = c.fetchall()
+    db.close()
+    return p
 
 def get_pop_articles():
     """Return a sorted list of articles that
     have been accessed the most. The most popular articles at the top.
 """
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("""
+    query = """
     SELECT articles.title, popular_articles.num
     FROM articles JOIN popular_articles
     ON articles.slug = popular_articles.slug
     ORDER BY popular_articles.num desc
     LIMIT 3;
-    """)
-    p = c.fetchall()
-    db.close()
-    return p
+    """
+    results = db_handler(query)
+    print "\nMost Popular 3 Articles of all time:"
+
+    for i in results:
+        print "- %s - %d Views" % (i[0], i[1])
 
 
 def get_pop_authors():
     """
     Return a sorted list with the most popular author at the top.
     """
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("""
+    query = """
     SELECT authors.name, pop_authors.sum
     FROM authors JOIN pop_authors
     ON authors.id = pop_authors.author;
-    """)
-    p = c.fetchall()
-    db.close()
-    return p
+    """
+    results = db_handler(query)
+    print "\nMost Popular Article Authors of all time:"
 
+    for i in results:
+        print "- %s - %d Views" % (i[0], i[1])
 
-def get_percentage():
+def get_err_percentages():
     """
     Return the days which more than 1% of the requests lead to errors.
 """
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("""
+    query = """
     SELECT to_char(date, 'FMMonth FMDD, YYYY'), error_percentage AS precentage
     FROM percentage_of_errors
     WHERE percentage_of_errors.error_percentage >= 1;
-    """)
-    p = c.fetchall()
-    db.close()
-    return p
+    """
+    results = db_handler(query)
+    print "\nDays with more than 1% of errors:"
 
+    for i in results:
+        print "- %s - %.2f%%" % (i[0], i[1])
 
-print "\nMost Popular 3 Articles of all time:"
+if __name__ == "__main__":
+    get_pop_articles()
+    get_pop_authors()
+    get_err_percentages()
 
-for i in get_pop_articles():
-    print "- %s - %d Views" % (i[0], i[1])
-
-print "\nMost Popular Article Authors of all time:"
-
-for i in get_pop_authors():
-    print "- %s - %d Views" % (i[0], i[1])
-
-print "\nDays with more than 1% of errors:"
-
-for i in get_percentage():
-    print "- %s - %.2f%%" % (i[0], i[1])
